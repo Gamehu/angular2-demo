218be421b266adb462d707034e1d21bbd8d21065
zhongzhong0505 <zt_zhong@qq.com>
2016-09-01 08:22:44 +0800

    提交更改

--- src/app/hero/hero-form.ts
+++ src/app/hero/hero-form.ts
@@ -4,6 +4,7 @@ import {Hero} from './hero';
 
 @Component({
     selector:'hero-form',
+    styleUrls:['./form.css'],
     templateUrl:'./hero-form.html'
 })
 
@@ -11,4 +12,11 @@ export class HeroFormComponent{
     powers = ['闪现','恢复','远程攻击'];
 
     model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+    newHero(){
+        this.model = new Hero(1,'','');
+    }
+    submitted  = false;
+    onSubmit(){
+       this.submitted = true;
+    }
 }
